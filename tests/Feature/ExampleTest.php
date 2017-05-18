de8585cd8d0d361d2c033064549ab99263d70036
sudaraka94 <sudarakayasindu@gmail.com>
2017-05-18 08:47:27 +0530

    Added some more tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,7 @@ use App\Client;
 use App\Http\Controllers\AdminController;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -56,4 +57,48 @@ class ExampleTest extends TestCase
         $col_point=Client::where('user_id','1');
         $this->assertNotNull($col_point);
     }
+
+//    this tests the homepage
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+//this checks the login
+    public function testLogin()
+    {
+        Auth::attempt(['email'=>'sudarakayasindu@gmail.com','password'=>'111111']);
+        $this->assertTrue(Auth::check());
+    }
+    //this checks a fake login
+    public function testFakeLogin()
+    {
+        Auth::attempt(['email'=>'fake@gmail.com','password'=>'fake123']);
+        $this->assertNotTrue(Auth::check());
+    }
+//this checks the logout
+    public function testLogout()
+    {
+        Auth::logout();
+        $this->assertNotTrue(Auth::check());
+    }
+    
+//    this checks the permissions for the application
+    public function testPermission()
+    {
+        Auth::attempt(['email'=>'sudarakayasindu@gmail.com','password'=>'111111']);
+        $response = $this->get('/add_garbage_record');
+        $response->assertStatus(302);
+    }
+
+    public function testAdminPermission()
+    {
+        Auth::logout();
+        Auth::attempt(['email'=>'vishaka@gmail.com','password'=>'111111']);
+        $response = $this->get('/add_user');
+        $response->assertStatus(302);
+    }
 }
+
+
